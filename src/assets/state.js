27bd9d969c1c5f83aa9b2b649d1db09e8e51b7f2
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-04-03 16:31:30 +0200

    problems on api

--- src/assets/state.js
+++ src/assets/state.js
@@ -1,5 +1,13 @@
 import { reactive } from "vue";
-//prima o poi servirà
+import axios from "axios";
+
 export const state = reactive({
-    
-    });
+    films: [],
+    getFilms() {
+        axios.get("https://api.themoviedb.org/3/movie/550?api_key=3c0747819cb9c3f045fb3c101a55e6e1")
+            .then(response => {
+                this.films = response.data;
+                console.log(response);
+            })
+    }
+});
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,36 @@
 <script>
+import { state } from '../assets/state.js'
+import axios from 'axios';
+
 export default {
-    name: 'AppHeader'
+  name: 'AppHeader',
+  data() {
+    return {
+      searchBarText: '',
+      filmList: [],
+    }
+  },
+  methods: {
+    searchBar() {
+      axios.get(`https://api.themoviedb.org/3/movie/550?api_key=3c0747819cb9c3f045fb3c101a55e6e1&query=${this.searchBarText}`)
+        .then(response => {
+          this.filmList = response.data;
+          console.log(response);
+          console.log(this.filmList);
+        })
+    },
+  }
 }
 </script>
 
 <template>
-  <h4>ciao</h4>
+  <input type="text" name="" id="" @keyup.enter="searchBar()" v-model="this.searchBarText">
+  <ul>
+    <li>titolo</li>
+    <li>titolo originale</li>
+    <li>Lingua</li>
+    <li>Voto</li>
+  </ul>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>

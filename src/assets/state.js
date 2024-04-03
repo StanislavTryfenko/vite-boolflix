cc0bfb6849a325adcb30284bf4790627ebf6641b
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-04-03 12:13:41 +0200

    Milestone 0

---
+++ src/assets/state.js
@@ -0,0 +1,5 @@
+import { reactive } from "vue";
+//prima o poi servirà
+export const state = reactive({
+    
+    });
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+  <h4>ciao</h4>
+</template>
+
+<style scoped>
+
+</style>

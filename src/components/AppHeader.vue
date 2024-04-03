d703034943b70a6586946e8cf9c6967aa347a238
StanislavTryfenko <stanislavtryfenko99@gmail.com>
2024-04-03 17:24:57 +0200

    Milestone 1

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -12,9 +12,18 @@ export default {
   },
   methods: {
     searchBar() {
-      axios.get(`https://api.themoviedb.org/3/movie/550?api_key=3c0747819cb9c3f045fb3c101a55e6e1&query=${this.searchBarText}`)
+      const options = {
+        method: 'GET',
+        url: 'https://api.themoviedb.org/3/search/movie',
+        params: { query: this.searchBarText },
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYzA3NDc4MTljYjljM2YwNDVmYjNjMTAxYTU1ZTZlMSIsInN1YiI6IjY2MGQwZjJmYzhhNWFjMDE3YzdhZDY0ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.2hm9xILiqLXjZ-QbnYhNfhSVDLUSlSz85smKDgNCnW0'
+        }
+      }
+      axios.request(options)
         .then(response => {
-          this.filmList = response.data;
+          this.filmList = response.data.results;
           console.log(response);
           console.log(this.filmList);
         })
@@ -25,11 +34,11 @@ export default {
 
 <template>
   <input type="text" name="" id="" @keyup.enter="searchBar()" v-model="this.searchBarText">
-  <ul>
-    <li>titolo</li>
-    <li>titolo originale</li>
-    <li>Lingua</li>
-    <li>Voto</li>
+  <ul v-for="(film, id) in filmList" :key="id">
+    <li>titolo {{ film.title }}</li>
+    <li>titolo originale {{ film.original_title }}</li>
+    <li>Lingua {{ film.original_language }}</li>
+    <li>Voto {{ film.vote_average }}</li>
   </ul>
 </template>
 
